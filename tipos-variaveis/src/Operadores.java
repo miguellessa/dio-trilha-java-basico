33ed788f655f1f54b7a52b31ec5cdeda647f1c69
Miguel Lessa <miguellessa3@gmail.com>
2025-03-31 11:23:45 -0300

    exemplo de operador ternario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,8 +1,17 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + " JAVA";
-        System.out.println(nomeCompleto);    
+
+        // condição ternaria ex:
+        
+        int a, b;
+
+        a= 6;
+        b= 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
     }
     
 }
